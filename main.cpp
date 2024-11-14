148261ac7f633ec62c6cd23e909772ab6aea46d3
azamjon1603 <azamjonolimjonov@icloud.com>
2024-11-14 14:29:23 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int son1, son2, yigindi;
+
+    cout << "Birinchi sonni kiriting: ";
+    cin >> son1;
+    cout << "Ikkinchi sonni kiriting: ";
+    cin >> son2;
+
+    yigindi = son1 + son2;
+
+    cout << "Natija: " << yigindi << endl;
+
+    return 0;
+}
